13ceb8d335bac57dc01f1ab03076e4d2dd5415ff
Sergiu Mosanu <sm7ed@virginia.edu>
2021-05-05 14:12:18 -0400

    cache by the book with hit rd/wr states split

--- SysVerilog_Model/cache.sv
+++ SysVerilog_Model/cache.sv
@@ -1,196 +1,138 @@
-// Created by fizzim.pl version 5.20 on 2020:10:06 at 16:33:52 (www.fizzim.com)
+// Created by fizzim.pl version 5.20 on 2021:03:17 at 13:54:33 (www.fizzim.com)
 
-module cache #(
-  parameter CHWIDTH = 5,
-  parameter ADDRWIDTH = 17,
-  
-  localparam CHROWS = 2**CHWIDTH,
-  localparam ROWS = 2**ADDRWIDTH
-  )
-  (
-  output logic [CHWIDTH-1:0] cRowId,
+module cache (
+  output logic [4:0] cRowId,
+  output logic dummy,
   output logic hold,
+  output logic ready,
   input logic RD,
-  input logic [ADDRWIDTH-1:0] RowId,
+  input logic [16:0] RowId,
   input logic WR,
   input logic clk,
   input logic rst,
   input logic sync
-  );
-  
-  typedef struct packed {
-    logic valid;
-    logic dirty;
-    logic [CHWIDTH-1:0] age; // for LRU
-    logic [CHWIDTH-1:0] tag;
-    logic [ADDRWIDTH-1:0] rowaddr;
-    logic [64-1:0] addr;
-  } cache_tag_type;
-  
-  cache_tag_type cache_tag [0:CHROWS-1];
-  genvar idx;
-  generate
-    for (idx=0; idx<CHROWS; idx++) begin
-      initial cache_tag[idx].valid=0;
-      initial cache_tag[idx].dirty=0;
-      initial cache_tag[idx].age='0;
-      initial cache_tag[idx].tag=idx;
-      initial cache_tag[idx].rowaddr='0;
-      initial cache_tag[idx].addr='0;
-    end
-  endgenerate
-  logic hit=0, miss=0, newrow=0;
-  
+);
+
   // state bits
   enum logic [2:0] {
-    Idle    = 3'b000, 
-    RDCheck = 3'b001, 
-    RDMiss  = 3'b010, 
-    READ    = 3'b011, 
-    WRCheck = 3'b100, 
-    WRITE   = 3'b101, 
-    WRMiss  = 3'b110
+    Idle       = 3'b000, 
+    Allocate   = 3'b001, 
+    CompareTag = 3'b010, 
+    WriteBack  = 3'b011, 
+    hitRD      = 3'b100, 
+    hitWR      = 3'b101
   } state, nextstate;
-  
-  
+
+
   // comb always block
   always_comb begin
     nextstate = state; // default to hold value because implied_loopback is set
     case (state)
-      Idle   : begin
-        if (WR) begin
-          nextstate = WRCheck;
+      Idle      : begin // wait for request from BankFSM
+        if (RD || WR) begin
+          nextstate = CompareTag;
+        end
+        else begin
+          nextstate = Idle;
+        end
+      end
+      Allocate  : begin // fetch block from memory
+        if (sync) begin
+          nextstate = CompareTag;
+        end
+        else if (!sync) begin
+          nextstate = Allocate;
+        end
+      end
+      CompareTag: begin // determine hit or miss
+        if (hit && RD) begin
+          nextstate = hitRD;
+        end
+        else if (hit && WR) begin
+          nextstate = hitWR;
+        end
+        else if (miss && dirty) begin
+          nextstate = WriteBack;
+        end
+        else if (miss && !dirty) begin
+          nextstate = Allocate;
+        end
+      end
+      WriteBack : begin // write data to memory
+        if (sync) begin
+          nextstate = Allocate;
+        end
+        else if (!sync) begin
+          nextstate = WriteBack;
+        end
+      end
+      hitRD     : begin // data read
+        if (!RD) begin
+          nextstate = Idle;
         end
         else if (RD) begin
-          nextstate = RDCheck;
+          nextstate = hitRD;
         end
-        else begin
+      end
+      hitWR     : begin // data write
+        if (!WR) begin
           nextstate = Idle;
         end
-      end
-      RDCheck: begin
-        if (miss) begin
-          nextstate = RDMiss;
-        end
-        else if (hit) begin
-          nextstate = READ;
-        end
-      end
-      RDMiss : begin
-        if (sync) begin
-          nextstate = READ;
-        end
-        else begin
-          nextstate = RDMiss;
-        end
-      end
-      READ   : begin
-        begin
-          nextstate = Idle;
-        end
-      end
-      WRCheck: begin
-        if (miss) begin
-          nextstate = WRMiss;
-        end
-        else if (hit) begin
-          nextstate = WRITE;
-        end
-      end
-      WRITE  : begin
-        begin
-          nextstate = Idle;
-        end
-      end
-      WRMiss : begin
-        if (sync) begin
-          nextstate = WRITE;
-        end
-        else begin
-          nextstate = WRMiss;
+        else if (WR) begin
+          nextstate = hitWR;
         end
       end
     endcase
   end
-  
+
   // Assign reg'd outputs to state bits
-  
+
   // sequential always block
   always_ff @(posedge clk or posedge rst) begin
     if (rst)
-    state <= Idle;
+      state <= Idle;
     else
-    state <= nextstate;
+      state <= nextstate;
   end
-  
+
   // datapath sequential always block
   always_ff @(posedge clk or posedge rst) begin
     if (rst) begin
-      cRowId[4:0] <= '0;
+      cRowId[4:0] <= 5'b0;
+      dummy <= 0;
       hold <= 0;
-      hit <= 0;
-      newrow <= 0;
+      ready <= 0;
     end
     else begin
+      cRowId[4:0] <= 5'b0; // default
+      dummy <= 0; // default
       hold <= 0; // default
+      ready <= 0; // default
       case (nextstate)
-        Idle   : begin
-          cRowId[4:0] <= '0;
-          hit <= 0;
-          miss <= 0;
-          newrow <= 0;
+        Idle      : begin
+          dummy <= 1;
         end
-        RDCheck: begin
-          for (int i = 0; i < CHROWS; i++) begin
-            // look for the row in the emulation memory cache
-            if((RowId == cache_tag[i].rowaddr) && (cache_tag[i].valid)) begin
-              cRowId <= cache_tag[i].tag;
-              hit = 1;
-            end
-          end
-          // row not found in emulation memory cache
-          miss <= !hit;
-        end
-        RDMiss : begin
+        Allocate  : begin
+          dummy <= 1;
           hold <= 1;
         end
-        READ   : begin
-          
+        CompareTag: begin
+          cRowId[4:0] <= RowID;
+          dummy <= 1;
         end
-        WRCheck: begin
-          if(newrow) begin
-            // look for an empty new row
-            for (int i=CHROWS; i>0; i--) begin
-              if(!cache_tag[i-1].valid) begin
-                cRowId <= cache_tag[i-1].tag;
-                hit = 1;
-              end
-            end
-            // no empty new row found
-            miss <= !hit;
-          end
-          else
-          for (int i = 0; i < CHROWS; i++) begin
-            // check if the row is already there
-            if((RowId == cache_tag[i].rowaddr) && (cache_tag[i].valid)) begin
-              cRowId <= cache_tag[i].tag;
-              hit <= 1;
-            end
-            else // new row is needed
-            newrow <= 1;
-          end
-        end
-        WRMiss : begin
+        WriteBack : begin
+          dummy <= 1;
           hold <= 1;
         end
-        WRITE  : begin
-          for (int i = 0; i < CHROWS; i++) begin
-            if(cRowId == cache_tag[i].tag) begin
-              cache_tag[i].valid <= 1;
-              cache_tag[i].dirty <= 1;
-              cache_tag[i].rowaddr <= RowId;
-            end
-          end
+        hitRD     : begin
+          cRowId[4:0] <= cRowId;
+          dummy <= 1;
+          ready <= 1;
+        end
+        hitWR     : begin
+          cRowId[4:0] <= cRowID;
+          dummy <= 1;
+          ready <= 1;
         end
       endcase
     end
