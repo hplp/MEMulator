87e7422dd1bf0da6034f573e852759708397bec1
Sergiu Mosanu <sm7ed@virginia.edu>
2021-08-11 16:11:01 -0400

    complete implementation with new UpdateTag state

--- SysVerilog_Model/cache.sv
+++ SysVerilog_Model/cache.sv
@@ -1,4 +1,3 @@
-// Created by fizzim.pl version 5.20 on 2021:07:26 at 17:12:21 (www.fizzim.com)
 
 module MEMSync #(
   parameter CHWIDTH = 6,
@@ -8,55 +7,41 @@ module MEMSync #(
   localparam ROWS = 2**ADDRWIDTH
   )
   (
-  output logic [CHWIDTH-1:0] LRU,
-  output logic [CHWIDTH-1:0] cRowId,
+  output logic [CHWIDTH-1:0] cRowId,  // Current MEM row to be used
   output logic dirty,
   output logic hit,
-  output logic load,
+  output logic [CHWIDTH-1:0] nRowId,  // Next MEM row to be used
   output logic ready,
-  output logic stall,
-  output logic store,
+  output logic stall,                 // stall signal
   input logic RD,
   input logic [ADDRWIDTH-1:0] RowId,
   input logic WR,
   input logic clk,
   input logic rst,
   input logic sync
-);
-
-typedef struct packed {
-  logic valid;
-  logic dirty;
-  logic [CHWIDTH-1:0] age; // for LRU
-  logic [CHWIDTH-1:0] tag;
-  logic [ADDRWIDTH-1:0] rowaddr;
-  logic [64-1:0] addr;
-} tag_table_type;
-
-tag_table_type tag_tbl [0:CHROWS-1];
-genvar idx;
-generate
-  for (idx=0; idx<CHROWS; idx++) begin
-    initial tag_tbl[idx].valid=0;
-    initial tag_tbl[idx].dirty=0;
-    initial tag_tbl[idx].age=(CHROWS-idx-1);
-    initial tag_tbl[idx].tag=idx;
-    initial tag_tbl[idx].rowaddr='0;
-    initial tag_tbl[idx].addr='0;
-  end
-endgenerate
-
+  );
+  
+  typedef struct packed {
+    logic valid;
+    logic dirty;
+    logic [CHWIDTH-1:0] tag; // id of rows in memory model
+    logic [ADDRWIDTH-1:0] rowaddr; // id of rows in memory
+  } tag_table_type;
+  
+  tag_table_type tag_tbl [CHROWS];
+  
   // state bits
   enum logic [2:0] {
     Idle       = 3'b000, 
     Allocate   = 3'b001, 
     CompareTag = 3'b010, 
-    WriteBack  = 3'b011, 
-    hitRD      = 3'b100, 
-    hitWR      = 3'b101
+    UpdateTag  = 3'b011, 
+    WriteBack  = 3'b100, 
+    hitRD      = 3'b101, 
+    hitWR      = 3'b110
   } state, nextstate;
-
-
+  
+  
   // comb always block
   always_comb begin
     nextstate = state; // default to hold value because implied_loopback is set
@@ -78,10 +63,15 @@ endgenerate
         else if (hit && WR) begin
           nextstate = hitWR;
         end
-        else if (!hit && dirty) begin
+        else if (!hit) begin
+          nextstate = UpdateTag;
+        end
+      end
+      UpdateTag : begin
+        if (dirty) begin
           nextstate = WriteBack;
         end
-        else if (!hit && !dirty) begin
+        else if (!dirty) begin
           nextstate = Allocate;
         end
       end
@@ -102,76 +92,65 @@ endgenerate
       end
     endcase
   end
-
+  
   // Assign reg'd outputs to state bits
-
+  
   // sequential always block
   always_ff @(posedge clk or posedge rst) begin
     if (rst)
-      state <= Idle;
+    state <= Idle;
     else
-      state <= nextstate;
+    state <= nextstate;
   end
-
+  
   // datapath sequential always block
   always_ff @(posedge clk or posedge rst) begin
     if (rst) begin
-      LRU[CHWIDTH-1:0] <= 0;
+      // reset tag table values
+      for (int i = 0; i < CHROWS; i++) begin
+        tag_tbl[i].valid=0;
+        tag_tbl[i].dirty=0;
+        tag_tbl[i].tag=i;
+        tag_tbl[i].rowaddr='0;
+      end
       cRowId[CHWIDTH-1:0] <= 0;
       dirty <= 0;
       hit <= 0;
-      load <= 0;
+      nRowId[CHWIDTH-1:0] <= 0;
       ready <= 0;
       stall <= 0;
-      store <= 0;
     end
     else begin
-      // where to allocate? find LRU
-      for (int i = 0; i < CHROWS; i++) begin
-        if(tag_tbl[i].age>tag_tbl[cRowId].age) begin
-          LRU <= tag_tbl[i].tag;
-        end
-      end
-      cRowId <= cRowId; // default
+      cRowId[CHWIDTH-1:0] <= cRowId; // default
       dirty <= 0; // default
       hit <= 0; // default
-      load <= 0; // default
+      nRowId[CHWIDTH-1:0] <= nRowId; // default
       ready <= 0; // default
       stall <= 0; // default
-      store <= 0; // default
       case (nextstate)
-        Idle      : begin
-          load <= RD;
-          store <= WR;
-        end
         Allocate  : begin
-          cRowId[CHWIDTH-1:0] <= LRU;
-          load <= load;
           stall <= 1;
-          store <= store;
           tag_tbl[cRowId].valid <= 1;
           tag_tbl[cRowId].rowaddr <= RowId;
-          tag_tbl[cRowId].age <= 0;
         end
         CompareTag: begin
-          load <= load;
           for (int i = 0; i < CHROWS; i++) begin
-            // look for the RowId in the emulation memory cache
+            // look for the RowId in the emulation memory tag table
             if((RowId == tag_tbl[i].rowaddr) && (tag_tbl[i].valid == 1)) begin
               // this tag_tbl_i rowaddr equals RowId and is valid
               cRowId <= tag_tbl[i].tag; // will focus on this row
-              tag_tbl[i].age <= 0; // using -> is Most Recently Used
               hit <= 1;
             end
-            // else begin // tag_tbl_i rowaddr =/= RowId or not valid
-            // end
           end
-          store <= store;
+        end
+        UpdateTag : begin
+          cRowId[CHWIDTH-1:0] <= nRowId;
+          dirty <= tag_tbl[nRowId].dirty;
+          nRowId[CHWIDTH-1:0] <= nRowId+1;
         end
         WriteBack : begin
-          load <= load;
           stall <= 1;
-          store <= store;
+          tag_tbl[cRowId].dirty <= 0;
         end
         hitRD     : begin
           ready <= 1;
--- SysVerilog_Model/testbnch_Cache.sv
+++ SysVerilog_Model/testbnch_Cache.sv
@@ -3,7 +3,7 @@
 module cachetestbench(
        );
        
-       parameter CHWIDTH = 5;
+       parameter CHWIDTH = 6;
        parameter ADDRWIDTH = 17;
        
        localparam CHROWS = 2**CHWIDTH;
@@ -15,19 +15,19 @@ module cachetestbench(
        reg rst;
        
        reg [CHWIDTH-1:0] cRowId;
-       reg hold;
+       reg stall;
        reg RD;
        reg [ADDRWIDTH-1:0] RowId;
        reg WR;
        reg sync;
        
-       cache #(
+       MEMSync #(
        .CHWIDTH(CHWIDTH),
        .ADDRWIDTH(ADDRWIDTH)
        ) 
        dut (
        .cRowId(cRowId),
-       .hold(hold),
+       .stall(stall),
        .RD(RD),
        .RowId(RowId),
        .WR(WR),
@@ -42,65 +42,92 @@ module cachetestbench(
        
        initial
        begin
-              rst = 1;
-              clk = 1;
-              sync = 0;
-              RD = 0;
+              clk=1;
+              rst=1;
+              sync=0;
+              RD=0;
               WR=0;
               RowId=0;
-              #tCK
+              #tCK;
               
-              rst = 0;
-              #tCK
+              rst=0;
+              #tCK;
               
-              // write then read
+              // repeatedly write, write then read it back
               for (i=0; i<CHROWS; i=i+1) begin
                      WR=1;
                      RowId=$urandom;
-                     #tCK
-                     #tCK
-                     #tCK
+                     #(3*tCK)
+                     
+                     sync=1;
+                     #tCK;
+                     sync=0;
+                     #(3*tCK)
                      
                      WR=0;
                      #tCK;
+
+                     WR=1;
+                     #(3*tCK)
                      
-                     RD=1;
-                     // RowId=RowId+i;
-                     #tCK
-                     #tCK
-                     #tCK
-                     
-                     RD=0;
+                     WR=0;
                      #tCK;
-                     $display(i);
+                     RD=1;
+                     #(3*tCK)
+                     RD=0;
+                     RowId=0;
+                     #tCK;
               end
-              
-              // induce a write miss followed by synq
-              WR=1;
-              RowId=$urandom;
-              #tCK
-              #tCK
-              #tCK
-              sync=1;
-              #tCK;
-              
-              WR=0;
-              sync=0;
-              #tCK;
-              
-              // induce a read miss followed by synq
-              RD=1;
-              RowId=$urandom;
-              #tCK
-              #tCK
-              #tCK
-              sync=1;
-              #tCK;
-              
-              RD=0;
-              sync=0;
-              #tCK;
-              
+
+              // cache may be full now so test WriteBack
+              for (i=0; i<8; i=i+1) begin
+                     WR=1;
+                     RowId=$urandom;
+                     #(3*tCK)
+                     
+                     sync=1;
+                     #tCK;
+                     sync=0;
+                     #(3*tCK)
+
+                     sync=1;
+                     #tCK;
+                     sync=0;
+                     #(3*tCK)
+                     
+                     WR=0;
+                     #tCK;
+
+                     WR=0;
+                     #tCK;
+                     RD=1;
+                     #(3*tCK)
+                     RD=0;
+                     RowId=0;
+                     #tCK;
+              end
+
+              // cache may be full now so test WriteBack with RD
+              for (i=0; i<8; i=i+1) begin
+                     RD=1;
+                     RowId=$urandom;
+                     #(3*tCK)
+
+                     sync=1;
+                     #tCK;
+                     sync=0;
+                     #(1*tCK)
+
+                     sync=1;
+                     #tCK;
+                     sync=0;
+                     #(3*tCK)
+
+                     RD=0;
+                     RowId=0;
+                     #tCK;
+              end
+
               #(4*tCK)
               $stop;
        end;
