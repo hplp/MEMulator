7dfc7873da348200506996055b164093d9cd4f4a
Sakib <ms3xy@crabtree.cs.virginia.edu>
2021-08-11 16:32:34 -0400

    some test bench changes

--- SysVerilog_Model/testbnch_TimingFSM.sv
+++ SysVerilog_Model/testbnch_TimingFSM.sv
@@ -37,138 +37,113 @@ module testbnch_TimingFSM(
        // define clk behavior
        always #(tCK*0.5) clk = ~clk;
        
-       integer i, j; // loop variable
+       integer i, j; // loop variable>
+       CFG = 0;
+       CKEH = 0;
+       CKEL = 0;
+       DPD = 0;
+       DPDX = 0;
+       MRR = 0;
+       MRW = 0;
+       PD = 0;
+       PDX = 0;
+       PR = 0;
+       PRA = 0;
+       RD = 0;
+       RDA = 0;
+       REF = 0;
+       SRF = 0;
+       WR = 0;
+       WRA = 0;
+       #tCK;
        
-       initial
+       // reset
+       reset_n = 1;
+       #(tCK*3);
+       
+       // activating a row in bank 1 in bank group 1
+       ACT = 1;
+       bg = 1;
+       ba = 1;
+       #tCK;>
+       //ACT = 1;
+       //#tCK;
+       //ACT = 0;
+       //#(tCK*15); // tRCD
+       //`endif
+       
+       // read
+       for (i = 0; i < BL; i = i + 1)
        begin
-              // initialize all inputs
-              clk = 1;
-              reset_n = 0;
-              bg = 0;
-              ba = 0;
-              ACT = 0;
-              BST = 0;
-              CFG = 0;
-              CKEH = 0;
-              CKEL = 0;
-              DPD = 0;
-              DPDX = 0;
-              MRR = 0;
-              MRW = 0;
-              PD = 0;
-              PDX = 0;
-              PR = 0;
-              PRA = 0;
-              RD = 0;
-              RDA = 0;
-              REF = 0;
-              SRF = 0;
-              WR = 0;
-              WRA = 0;
               #tCK;
-              
-              // reset
-              reset_n = 1;
-              #(tCK*3);
-
-              // activating a row in bank 1 in bank group 1
-              ACT = 1;
-              bg = 1;
-              ba = 1;
-              #tCK;
-              ACT = 0;
-              #(tCK*15); // tRCD
-              #(tCK*18); // tCL
-              
-              // write
-              #tCK;
-              for (i = 0; i < BL; i = i + 1)
-              begin
-                     WR = (i==0)? 1 : 0;
-                     #tCK;
-              end
-               #tCK;
-
-              //`ifdef RowClone
-              //#(tCK*5); // activating again for RowClone
-              //ACT = 1;
-              //#tCK;
-              //ACT = 0;
-              //#(tCK*15); // tRCD
-              //`endif
-              
-              // read
-              for (i = 0; i < BL; i = i + 1)
-              begin
-                     #tCK;
-                     RD = (i==0)? 1 : 0;
-              end
-
-              // precharge and back to idle
-              #tCK;
-              PR = 1;
-              #tCK;
-              PR = 0;
-              #(16*tCK)
-
-              // refresh
-              REF = 1;
-              #tCK;
-              REF = 0;
-              #(34*tCK);
-
-              bg = 0;
-              ba = 0;
-              #(4*tCK);
-
-              // precharge and back to idle
-              #tCK;
-              PR = 1;
-              #tCK;
-              PR = 0;
-              #(16*tCK)
-
-              // activating a row in bank 1 in bank group 1
-              ACT = 1;
-              bg = 1;
-              ba = 1;
-              #tCK;
-              ACT = 0;
-              #(tCK*15); // tRCD
-              #(tCK*18); // tCL
-
-              // write Auto-Precharge
-              #tCK;
-              for (i = 0; i < BL; i = i + 1)
-              begin
-                     WRA = (i==0)? 1 : 0;
-                     #tCK;
-              end
-
-              // precharge and back to idle
-              #tCK;
-              PR = 1;
-              #tCK;
-              PR = 0;
-              #(16*tCK)
-
- 	      // activating a row in bank 1 in bank group 1
-              ACT = 1;
-              bg = 1;
-              ba = 1;
-              #tCK;
-              ACT = 0;
-              #(tCK*15); // tRCD
-              #(tCK*18); // tCL
-
-              // read Auto-Precharge
-              for (i = 0; i < BL; i = i + 1)
-              begin
-                     #tCK;
-                     RDA = (i==0)? 1 : 0;
-              end
-           
-              $stop;
-       end;
+              RD = (i==0)? 1 : 0;
+       end
        
+       // precharge and back to idle
+       #tCK;
+       PR = 1;
+       #tCK;
+       PR = 0;
+       #(16*tCK)
+       
+       // refresh
+       REF = 1;
+       #tCK;
+       REF = 0;
+       #(34*tCK);
+       
+       bg = 0;
+       ba = 0;
+       #(4*tCK);
+       
+       // precharge and back to idle
+       #tCK;
+       PR = 1;
+       #tCK;
+       PR = 0;
+       #(16*tCK)
+       
+       // activating a row in bank 1 in bank group 1
+       ACT = 1;
+       bg = 1;
+       ba = 1;
+       #tCK;
+       ACT = 0;
+       #(tCK*15); // tRCD
+       #(tCK*18); // tCL
+       
+       // write Auto-Precharge
+       #tCK;
+       for (i = 0; i < BL; i = i + 1)
+       begin
+              WRA = (i==0)? 1 : 0;
+              #tCK;
+       end
+       
+       // precharge and back to idle
+       #tCK;
+       PR = 1;
+       #tCK;
+       PR = 0;
+       #(16*tCK)
+       
+       // activating a row in bank 1 in bank group 1
+       ACT = 1;
+       bg = 1;
+       ba = 1;
+       #tCK;
+       ACT = 0;
+       #(tCK*15); // tRCD
+       #(tCK*18); // tCL
+       
+       // read Auto-Precharge
+       for (i = 0; i < BL; i = i + 1)
+       begin
+              #tCK;
+              RDA = (i==0)? 1 : 0;
+       end
+       
+       $stop;
+end;
+
 endmodule
