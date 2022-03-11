8f60ed4ccd1aa24798f9500e4146e0cc36bfe12d
Sergiu Mosanu <sm7ed@virginia.edu>
2022-03-10 19:02:53 -0500

    sv logic, indent, assert display

--- SysVerilog_Model/Bank.sv
+++ SysVerilog_Model/Bank.sv
@@ -10,28 +10,28 @@
 //     the small number of full rows modeled are then mapped to any rows in use
 module Bank
   #(parameter DEVICE_WIDTH = 4,
-    parameter COLWIDTH = 10,
-    parameter CHWIDTH = 5,
-    localparam COLS = 2**COLWIDTH, // number of columns
-    localparam CHROWS = 2**CHWIDTH, // number of full rows allocated to a Bank model
-    localparam DEPTH = COLS*CHROWS) // amount of BRAM per Bank as full rows
+  parameter COLWIDTH = 10,
+  parameter CHWIDTH = 5,
+  localparam COLS = 2**COLWIDTH, // number of columns
+  localparam CHROWS = 2**CHWIDTH, // number of full rows allocated to a Bank model
+  localparam DEPTH = COLS*CHROWS) // amount of BRAM per Bank as full rows
   (
-    input  wire clk,
-    input  wire [0:0]              rd_o_wr,
-    input  wire [DEVICE_WIDTH-1:0] dqin,
-    output wire [DEVICE_WIDTH-1:0] dqout,
-    input  wire [CHWIDTH-1:0]      row,
-    input  wire [COLWIDTH-1:0]     column
-);
-
-    bank_array #(.WIDTH(DEVICE_WIDTH), .DEPTH(DEPTH)) arrayi (
-        .clk(clk),
-        .addr({row, column}),
-        .rd_o_wr(rd_o_wr), // 0->rd, 1->wr
-        .i_data(dqin),
-        .o_data(dqout)
-    );
-
+  input  logic clk,
+  input  logic [0:0]              rd_o_wr,
+  input  logic [DEVICE_WIDTH-1:0] dqin,
+  output logic [DEVICE_WIDTH-1:0] dqout,
+  input  logic [CHWIDTH-1:0]      row,
+  input  logic [COLWIDTH-1:0]     column
+  );
+  
+  bank_array #(.WIDTH(DEVICE_WIDTH), .DEPTH(DEPTH)) arrayi (
+  .clk(clk),
+  .addr({row, column}),
+  .rd_o_wr(rd_o_wr), // 0->rd, 1->wr
+  .i_data(dqin),
+  .o_data(dqout)
+  );
+  
 endmodule
 
 // old code that used to manage RAS and CAS for bursts
--- SysVerilog_Model/Chip.sv
+++ SysVerilog_Model/Chip.sv
@@ -11,29 +11,29 @@
 // one place while still achieving the goal of slicing and hierarchically
 // placing the data.
 module Chip
-  #(parameter BGWIDTH = 2,
+    #(parameter BGWIDTH = 2,
     parameter BAWIDTH = 2,
     parameter COLWIDTH = 10,
     parameter DEVICE_WIDTH = 4,
     parameter CHWIDTH = 5,
-
+    
     localparam BANKGROUPS = 2**BGWIDTH,
     localparam BANKSPERGROUP = 2**BAWIDTH,
     localparam COLS = 2**COLWIDTH
-)
-  (
-    input wire clk,
+    )
+    (
+    input logic clk,
     // SystemVerilog multi-dimentional arrays allows to easily scale the bundling
     // of inputs. For example, the data of bank 2 and bank group 3 can be read
     // out as dqout[3][2] and setting the row and column value will give the
     // relevant word
-    input wire  [0:0]             rd_o_wr [BANKGROUPS-1:0][BANKSPERGROUP-1:0],
-    input wire  [DEVICE_WIDTH-1:0]dqin    [BANKGROUPS-1:0][BANKSPERGROUP-1:0],
-    output wire [DEVICE_WIDTH-1:0]dqout   [BANKGROUPS-1:0][BANKSPERGROUP-1:0],
-    input wire  [CHWIDTH-1:0]     row     [BANKGROUPS-1:0][BANKSPERGROUP-1:0],
-    input wire  [COLWIDTH-1:0]    column  [BANKGROUPS-1:0][BANKSPERGROUP-1:0]
-);
-
+    input logic  [0:0]             rd_o_wr [BANKGROUPS-1:0][BANKSPERGROUP-1:0],
+    input logic  [DEVICE_WIDTH-1:0]dqin    [BANKGROUPS-1:0][BANKSPERGROUP-1:0],
+    output logic [DEVICE_WIDTH-1:0]dqout   [BANKGROUPS-1:0][BANKSPERGROUP-1:0],
+    input logic  [CHWIDTH-1:0]     row     [BANKGROUPS-1:0][BANKSPERGROUP-1:0],
+    input logic  [COLWIDTH-1:0]    column  [BANKGROUPS-1:0][BANKSPERGROUP-1:0]
+    );
+    
     // generating bank groups and assigning the corresponding inputs/outputs
     genvar bgi;
     generate
@@ -43,14 +43,14 @@ module Chip
             .COLWIDTH(COLWIDTH),
             .DEVICE_WIDTH(DEVICE_WIDTH),
             .CHWIDTH(CHWIDTH)) BGi (
-                .clk(clk),
-                .rd_o_wr(rd_o_wr[bgi]),
-                .dqin(dqin[bgi]),
-                .dqout(dqout[bgi]),
-                .row(row[bgi]),
-                .column(column[bgi])
+            .clk(clk),
+            .rd_o_wr(rd_o_wr[bgi]),
+            .dqin(dqin[bgi]),
+            .dqout(dqout[bgi]),
+            .row(row[bgi]),
+            .column(column[bgi])
             );
         end
     endgenerate
-
+    
 endmodule
